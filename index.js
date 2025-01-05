059c0830bcfe222ca9b5aed887a5f1a42d19d42b
Java_Scodeen_Global_Class-at-831274488909 <Java_Scodeen_Global_Class-at-831274488909>
2025-01-05 19:37:01 +0530

    feat: adding API's for users and user by id

---
+++ index.js
@@ -0,0 +1,63 @@
+const express=require("express")
+const cors=require("cors")
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+
+const users=[{
+    id:1,
+    username:"RahulSharma123",
+    name:"Rahul Sharma",
+    repoCount:98,
+    location:"Delhi",
+},
+{
+    id:2,
+    username:"AnkitaSingh789",
+    name:"Ankita Singh",
+    repoCount:149,
+    location:"Mumbai",
+},
+{
+    id:3,
+    username:"PritiJha456",
+    name:"Priti Jha",
+    repoCount:172,
+    location:"Kolkata",
+},
+{
+    id:4,
+    username:"RoyalMandela234",
+    name:"Royal Mandela",
+    repoCount:250,
+    location:"Delhi",
+},
+{
+    id:5,
+    username:"HPNaik987",
+    name:"HP Naik",
+    repoCount:178,
+    location:"Indore",
+}];
+
+app.get('/users',(req, res)=>{
+res.json({users});
+});
+
+app.get('/users/:id',(req, res)=>{
+let id=parseInt(req.params.id);
+let user=users.find(userObj=>userObj.id===id);
+if(user){
+    res.status(200).json({user})
+}
+else{
+res.status(404).json({message:"No Data Found"});
+}
+})
+
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
