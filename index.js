456bae838c884ae0879c9d92c0125613a37059e7
Java_Scodeen_Global_Class-at-831274488909 <Java_Scodeen_Global_Class-at-831274488909>
2025-01-06 14:23:04 +0530

    made some changes in index.js

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-const express=require("express")
-const cors=require("cors")
+const express = require("express")
+const cors = require("cors")
 
-const app=express()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const users=[{
-    id:1,
-    username:"RahulSharma123",
-    name:"Rahul Sharma",
-    repoCount:98,
-    location:"Delhi",
+const users = [{
+    id: 1,
+    username: "RahulSharma123",
+    name: "Rahul Sharma",
+    repoCount: 98,
+    location: "Delhi",
 },
 {
-    id:2,
-    username:"AnkitaSingh789",
-    name:"Ankita Singh",
-    repoCount:149,
-    location:"Mumbai",
+    id: 2,
+    username: "AnkitaSingh789",
+    name: "Ankita Singh",
+    repoCount: 149,
+    location: "Mumbai",
 },
 {
-    id:3,
-    username:"PritiJha456",
-    name:"Priti Jha",
-    repoCount:172,
-    location:"Kolkata",
+    id: 3,
+    username: "PritiJha456",
+    name: "Priti Jha",
+    repoCount: 172,
+    location: "Kolkata",
 },
 {
-    id:4,
-    username:"RoyalMandela234",
-    name:"Royal Mandela",
-    repoCount:250,
-    location:"Delhi",
+    id: 4,
+    username: "RoyalMandela234",
+    name: "Royal Mandela",
+    repoCount: 250,
+    location: "Delhi",
 },
 {
-    id:5,
-    username:"HPNaik987",
-    name:"HP Naik",
-    repoCount:178,
-    location:"Indore",
+    id: 5,
+    username: "HPNaik987",
+    name: "HP Naik",
+    repoCount: 178,
+    location: "Indore",
 }];
 
-app.get('/users',(req, res)=>{
-res.json({users});
+app.get('/users', (req, res) => {
+    res.json({ users });
 });
 
-app.get('/users/id/:id',(req, res)=>{
-let id=parseInt(req.params.id);
-let user=users.find(userObj=>userObj.id===id);
-if(user){
-    res.status(200).json({user})
-}
-else{
-res.status(404).json({message:"No Data Found with id:"+id});
-}
-})
-
-app.get('/users/username/:username',(req, res)=>{
-    let username=req.params.username;
-    let user=users.find(userObj=>userObj.username===username);
-    if(user){
-        res.status(200).json({user})
+app.get('/users/id/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let user = users.find(userObj => userObj.id === id);
+    if (user) {
+        res.status(200).json({ user })
     }
-    else{
-    res.status(404).json({message:"No Data Found with username: "+username});
+    else {
+        res.status(404).json({ message: "No Data Found with id:" + id });
     }
-    })
+});
 
-const PORT=3000;
-app.listen(PORT,()=>{
+app.get('/users/username/:username', (req, res) => {
+    let username = req.params.username;
+    let user = users.find(userObj => userObj.username === username);
+    if (user) {
+        res.status(200).json({ user })
+    }
+    else {
+        res.status(404).json({ message: "No Data Found with username: " + username });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
