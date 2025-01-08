eb34b85c0cf13fe9b83dce0a438f0f5faeff34e9
Java_Scodeen_Global_Class-at-831274488909 <Java_Scodeen_Global_Class-at-831274488909>
2025-01-08 22:26:05 +0530

    feat: Added all code files for project

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    use_env_variable: "DIRECT_URL", // Use direct connection for migrations
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    use_env_variable: "DATABASE_URL", // Use pooled connection for runtime
+  },
+};
